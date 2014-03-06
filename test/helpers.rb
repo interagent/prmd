5016d1f9faeca24bb7ca00e80d4f2fd0c14891a8
geemus <geemus@gmail.com>
2014-03-06 10:10:36 -0600

    cleaning up tests, starting to add sample data/schemas

---
+++ test/helpers.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "prmd"
+
+def input_schemas_path
+  @data_path ||= File.join(File.dirname(__FILE__), 'schemas', 'input')
+end
+
+def user_input_schema
+  @user_input_schema ||= Prmd::Schema.load(File.join(input_schemas_path, 'user.json'))
+end
--- test/schema_test.rb
+++ test/schema_test.rb
@@ -1,36 +1,13 @@
-require "minitest/autorun"
-require "prmd"
+require File.join(File.dirname(__FILE__), 'helpers')
 
 class SchemaTest < Minitest::Unit::TestCase
-  def setup
-    @schema = Prmd::Schema.new({
-      "$schema" => "http://json-schema.org/draft-04/schema",
-      "definitions" => {
-        "address" => {
-          "type" => "object",
-          "properties" => {
-            "street_address" => { "type" => "string" },
-            "city" => { "type" => "string" },
-            "state" => { "type" => "string" }
-          },
-          "required" => ["street_address", "city", "state"]
-        }
-      },
-      "type" => "object",
-      "properties" => {
-        "billing_address" => { "$ref" => "#/definitions/address" },
-        "shipping_address" => { "$ref" => "#/definitions/address" }
-      }
-    })
-  end
-
   def test_dereference_with_ref
-    ref = @schema.dereference(@schema.data["properties"]["billing_address"])
-    assert_equal ref, @schema.data["definitions"]["address"]
+    ref = user_input_schema.dereference({ "$ref" => "/schema/user#/definitions/id" })
+    assert_equal ref, user_input_schema.data["definitions"]["id"]
   end
 
   def test_dereference_without_ref
-    ref = @schema.dereference(@schema.data["definitions"]["address"])
-    assert_equal ref, @schema.data["definitions"]["address"]
+    ref = user_input_schema.dereference("/schema/user#/definitions/id")
+    assert_equal ref, user_input_schema.data["definitions"]["id"]
   end
 end
