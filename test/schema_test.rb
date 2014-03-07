d66e6a70d45345166b1845566626401414af75e6
geemus <geemus@gmail.com>
2014-03-07 09:57:35 -0600

    fix tests to use new nicer schema accessors

--- test/schema_test.rb
+++ test/schema_test.rb
@@ -3,11 +3,11 @@ require File.join(File.dirname(__FILE__), 'helpers')
 class SchemaTest < Minitest::Unit::TestCase
   def test_dereference_with_ref
     ref = user_input_schema.dereference({ "$ref" => "/schema/user#/definitions/id" })
-    assert_equal ref, user_input_schema.data["definitions"]["id"]
+    assert_equal ref, user_input_schema["definitions"]["id"]
   end
 
   def test_dereference_without_ref
     ref = user_input_schema.dereference("/schema/user#/definitions/id")
-    assert_equal ref, user_input_schema.data["definitions"]["id"]
+    assert_equal ref, user_input_schema["definitions"]["id"]
   end
 end
