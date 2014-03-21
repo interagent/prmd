6d8c5adc89dfa16829ecf2808fcf11db841c2f8f
Olivier Lance <olivier@digitalcuisine.fr>
2014-03-22 00:12:28 +0100

    Added a test helper to test for any raised exception of given class

--- test/helpers.rb
+++ test/helpers.rb
@@ -8,3 +8,13 @@ end
 def user_input_schema
   @user_input_schema ||= Prmd::Schema.load(File.join(input_schemas_path, 'user.json'))
 end
+
+## Helper to check for any thrown error
+def assert_raises_any(exception_kind)
+  e = nil
+  begin
+    yield
+  rescue => e
+  end
+  e.must_be_kind_of exception_kind
+end
