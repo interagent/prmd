3014a5c48580630f7ba4dae68a1af576c3d78b55
geemus <geemus@gmail.com>
2014-05-02 13:15:57 -0500

    [doc] move identifiers to template

--- lib/prmd/commands/doc.rb
+++ lib/prmd/commands/doc.rb
@@ -4,19 +4,11 @@ module Prmd
       File.open(path, 'r').read + "\n"
     end
 
+    template_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'views', 'schemata.erb'))
+    template = File.read(template_path)
+
     doc << schema['definitions'].map do |_, schemata|
-      identifiers = if schemata['definitions'].has_key?('identity')
-        identity = schemata['definitions']['identity']
-        (identity['anyOf'] || [identity]).map {|ref| ref['$ref'].split('/').last }
-      else
-        []
-      end
-
-      template_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'views', 'schemata.erb'))
-      template = File.read(template_path)
-
       Erubis::Eruby.new(template).result({
-        identifiers:     identifiers,
         schema:          schema,
         schemata:        schemata,
         template_path:   template_path
