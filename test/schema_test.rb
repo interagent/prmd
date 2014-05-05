147f51a0f1ca718e3a7c9a4874b2ad74842c8b09
geemus <geemus@gmail.com>
2014-05-05 13:57:19 -0500

    fix tests to match new combine functionality

--- test/schema_test.rb
+++ test/schema_test.rb
@@ -3,32 +3,32 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'helpers'))
 class SchemaTest < Minitest::Unit::TestCase
   def test_dereference_with_ref
     key, value = user_input_schema.dereference({
-      '$ref' => '#/definitions/user/definitions/id'
+      '$ref' => '#/definitions/schemata/user/definitions/id'
     })
-    assert_equal(key,   '#/definitions/user/definitions/id')
-    assert_equal(value, user_input_schema['definitions']['user']['definitions']['id'])
+    assert_equal(key,   '#/definitions/schemata/user/definitions/id')
+    assert_equal(value, user_input_schema['definitions']['schemata']['user']['definitions']['id'])
   end
 
   def test_dereference_without_ref
-    key, value = user_input_schema.dereference('#/definitions/user/definitions/id')
-    assert_equal(key,   '#/definitions/user/definitions/id')
-    assert_equal(value, user_input_schema['definitions']['user']['definitions']['id'])
+    key, value = user_input_schema.dereference('#/definitions/schemata/user/definitions/id')
+    assert_equal(key,   '#/definitions/schemata/user/definitions/id')
+    assert_equal(value, user_input_schema['definitions']['schemata']['user']['definitions']['id'])
   end
 
   def test_dereference_with_nested_ref
     key, value = user_input_schema.dereference({
-      '$ref' => '#/definitions/user/definitions/identity'
+      '$ref' => '#/definitions/schemata/user/definitions/identity'
     })
-    assert_equal(key,   '#/definitions/user/definitions/id')
-    assert_equal(value, user_input_schema['definitions']['user']['definitions']['id'])
+    assert_equal(key,   '#/definitions/schemata/user/definitions/id')
+    assert_equal(value, user_input_schema['definitions']['schemata']['user']['definitions']['id'])
   end
 
   def test_dereference_with_local_context
     key, value = user_input_schema.dereference({
-      '$ref'     => '#/definitions/user/properties/id',
+      '$ref'     => '#/definitions/schemata/user/properties/id',
       'override' => true
     })
-    assert_equal(key,   '#/definitions/user/definitions/id')
-    assert_equal(value, {'override' => true}.merge(user_input_schema['definitions']['user']['definitions']['id']))
+    assert_equal(key,   '#/definitions/schemata/user/definitions/id')
+    assert_equal(value, {'override' => true}.merge(user_input_schema['definitions']['schemata']['user']['definitions']['id']))
   end
 end
