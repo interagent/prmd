4b6b2b9f9b183db92a82642e974c255323aa8b86
Justin Halsall <justin@juice10.com>
2014-07-16 18:58:17 +0200

    refactor everything into subclasses

--- lib/prmd/url_generator.rb
+++ lib/prmd/url_generator.rb
@@ -9,74 +9,74 @@ module Prmd
 
     def url_params
       if @options[:style].downcase == 'json'
-        self.extend(JSON)
+        klass = Generators::JSON
       else
-        self.extend(Default)
+        klass = Generators::Default
       end
 
-      generate({schema: @schema, link: @link})
+      klass.new.generate({schema: @schema, link: @link})
     end
 
     private
 
-    module Default
-      def generate(params)
-        # @schema = params[:schema]
-        # @link = params[:link]
-        data = {}
-        data.merge!(@schema.schema_example(@link['schema']))
-        generate_params(data)
-      end
-
-      private
-
-      def param_name(key, prefix, array = false)
-        result = if prefix
-          "#{prefix}[#{key}]"
-        else
-          key
+    module Generators
+      class Default
+        def generate(params)
+          data = {}
+          data.merge!(params[:schema].schema_example(params[:link]['schema']))
+          generate_params(data)
         end
 
-        result += "[]" if array
-        result
-      end
+        private
 
-      def generate_params(obj, prefix = nil)
-        result = []
-        obj.each do |key,value|
-          if value.is_a?(Hash)
-            newprefix = if prefix
-              "#{prefix}[#{key}]"
-            else
-              key
-            end
-            result << generate_params(value, newprefix)
-          elsif value.is_a?(Array)
-            value.each do |val|
-              result << [param_name(key, prefix, true), CGI.escape(val.to_s)].join('=')
-            end
+        def param_name(key, prefix, array = false)
+          result = if prefix
+            "#{prefix}[#{key}]"
           else
-            next unless value # ignores parameters with empty examples
-            result << [param_name(key, prefix), CGI.escape(value.to_s)].join('=')
+            key
           end
+
+          result += "[]" if array
+          result
         end
-        result.flatten
+
+        def generate_params(obj, prefix = nil)
+          result = []
+          obj.each do |key,value|
+            if value.is_a?(Hash)
+              newprefix = if prefix
+                "#{prefix}[#{key}]"
+              else
+                key
+              end
+              result << generate_params(value, newprefix)
+            elsif value.is_a?(Array)
+              value.each do |val|
+                result << [param_name(key, prefix, true), CGI.escape(val.to_s)].join('=')
+              end
+            else
+              next unless value # ignores parameters with empty examples
+              result << [param_name(key, prefix), CGI.escape(value.to_s)].join('=')
+            end
+          end
+          result.flatten
+        end
       end
-    end
 
-    module JSON
-      def generate(params)
-        data = {}
-        data.merge!(@schema.schema_example(@link['schema']))
+      class JSON
+        def generate(params)
+          data = {}
+          data.merge!(params[:schema].schema_example(params[:link]['schema']))
 
-        result = []
-        data.sort_by {|k,_| k.to_s }.each do |key, values|
-          [values].flatten.each do |value|
-            result << [key.to_s, CGI.escape(value.to_s)].join('=')
+          result = []
+          data.sort_by {|k,_| k.to_s }.each do |key, values|
+            [values].flatten.each do |value|
+              result << [key.to_s, CGI.escape(value.to_s)].join('=')
+            end
           end
-        end
 
-        result
+          result
+        end
       end
     end
   end
