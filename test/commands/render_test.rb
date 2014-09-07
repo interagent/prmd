d920d29a74ac93ea3667a0b5c51c30ea4252ea2a
Jason Rudolph <github@jasonrudolph.com>
2014-09-07 17:27:52 -0400

    Add test to provide sanity check for Prmd#render
    Use the core parts of test/commands/verify_test.rb to define a basic
    sanity checking test for Prmd#render. For now, the test essentially
    verifies that Prmd#render successfully produced some Markdown. This will
    at least let us know that it didn't throw an exception. Over time, this
    test can grow to assert more specific results of Prmd#render.

---
+++ test/commands/render_test.rb
@@ -0,0 +1,78 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'helpers'))
+
+require "json_pointer"
+
+class InteragentRenderTest < Minitest::Test
+
+  def test_render_for_valid_schema
+    markdown = render
+
+    assert_match /An app in our PaaS ecosystem./, markdown
+  end
+
+  private
+
+  def data
+    @data ||= {
+      "$schema"     => "http://interagent.github.io/interagent-hyper-schema",
+      "description" => "My simple example API.",
+      "id"          => "http://example.com/schema",
+      "title"       => "Example API",
+      "definitions" => {
+        "app" => {
+          "description" => "An app in our PaaS ecosystem.",
+          "title" => "App",
+          "type" => "object",
+          "definitions" => {
+            "identity" => {
+              "anyOf" => [
+                {
+                  "$ref" => "#/definitions/app/definitions/name"
+                }
+              ]
+            },
+            "name" => {
+              "description" => "The app's name.",
+              "type"        => "string"
+            }
+          },
+          "links" => [
+            {
+              "description" => "Create a new app.",
+              "href" => "/apps",
+              "method" => "POST",
+              "rel" => "create",
+              "title" => "Create App"
+            }
+          ],
+          "properties" => {
+          }
+        }
+      },
+      "links" => [
+        {
+          "href" => "https://example.com",
+          "rel" => "self"
+        }
+      ],
+      "properties" => {
+        "app" => {
+          "$ref" => "#/definitions/app"
+        }
+      },
+      "type" => "object"
+    }
+  end
+
+  def pointer(path)
+    JsonPointer.evaluate(data, path)
+  end
+
+  def render
+    schema = Prmd::Schema.new(data)
+
+    template = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'prmd', 'templates'))
+
+    Prmd.render(schema, {:template => template})
+  end
+end
