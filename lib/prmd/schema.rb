f51a657714faa5f306ce5c41d1d4ce0d0c47b0b3
Paul Guelpa <paul@shopkeep.com>
2014-09-15 16:53:35 -0400

    Fix failing render_test spec

--- lib/prmd/schema.rb
+++ lib/prmd/schema.rb
@@ -60,7 +60,7 @@ module Prmd
       if value.has_key?('example')
         value['example']
       elsif value.has_key?('anyOf')
-        ref = value['anyOf'].detect {|ref| ref['$ref'].split('/').last == 'id'} || value['anyOf'].first
+        ref = value['anyOf'].detect {|ref| ref.has_key?('$ref') && ref['$ref'].split('/').last == 'id'} || value['anyOf'].first
         schema_example(ref)
       elsif value.has_key?('properties') # nested properties
         schema_example(value)
