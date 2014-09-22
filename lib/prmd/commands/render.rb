a9eb8c6e04f18d579bc3347d9d4577672481bbbe
Corey Powell <mistdragon100@gmail.com>
2014-09-22 11:26:44 -0500

    render will abort if :template key was not given

--- lib/prmd/commands/render.rb
+++ lib/prmd/commands/render.rb
@@ -10,12 +10,13 @@ module Prmd
       doc << options[:prepend].map {|path| File.read(path)}.join("\n") << "\n"
     end
 
-    template_dir = File::expand_path(options[:template])
+    template = options.fetch(:template) { abort "render: Template was not provided" }
+    template_dir = File.expand_path(template)
     if not File.directory?(template_dir)  # to keep backward compatibility
-      template_dir = File.dirname(options[:template])
+      template_dir = File.dirname(template)
     end
 
-    doc << Prmd::Template::render('schema.erb', template_dir, {
+    doc << Prmd::Template.render('schema.erb', template_dir, {
       options:         options,
       schema:          schema
     })
