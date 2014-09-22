b65aaac770a7b7ff022e8f63b0ee93f1946cfafc
Corey Powell <mistdragon100@gmail.com>
2014-09-22 09:33:46 -0500

    Break CLI.run into smaller more managable pieces
    Actually, quite a bit of work went into this.
    In addition to being able to write directly to a file using the --output-file option.

--- lib/prmd/cli.rb
+++ lib/prmd/cli.rb
@@ -1,18 +1,19 @@
 module Prmd
   module CLI
-    def self.run(argv)
+    def self.parse_options(argv, opts={})
+      binname = opts.fetch(:bin, "prmd")
       options = {}
 
       commands = {
         combine: OptionParser.new do |opts|
-          opts.banner = "prmd combine [options] <file or directory>"
-          opts.on("-m", "--meta meta.json", "Set defaults for schemata") do |m|
+          opts.banner = "#{binname} combine [options] <file or directory>"
+          opts.on("-m", "--meta META", String, "Set defaults for schemata") do |m|
             options[:meta] = m
           end
         end,
         doc: OptionParser.new do |opts|
-          opts.banner = "prmd doc [options] <combined schema>"
-          opts.on("-s", "--settings prmd-config.json", String, "Config file to use") do |s|
+          opts.banner = "#{binname} doc [options] <combined schema>"
+          opts.on("-s", "--settings CONFIG", String, "Config file to use") do |s|
             settings = YAML.load_file(s) || {}
             options = HashHelpers.deep_symbolize_keys(settings).merge(options)
           end
@@ -24,13 +25,13 @@ module Prmd
           end
         end,
         init: OptionParser.new do |opts|
-          opts.banner = "prmd init [options] <resource name>"
+          opts.banner = "#{binname} init [options] <resource name>"
           opts.on("-y", "--yaml", "Generate YAML") do |y|
             options[:yaml] = y
           end
         end,
         render: OptionParser.new do |opts|
-          opts.banner = "prmd render [options] <combined schema>"
+          opts.banner = "#{binname} render [options] <combined schema>"
           opts.on("-p", "--prepend header,overview", Array, "Prepend files to output") do |p|
             options[:prepend] = p
           end
@@ -39,7 +40,7 @@ module Prmd
           end
         end,
         verify: OptionParser.new do |opts|
-          opts.banner = "prmd verify [options] <combined schema>"
+          opts.banner = "#{binname} verify [options] <combined schema>"
         end
       }
 
@@ -54,69 +55,109 @@ module Prmd
           puts "prmd #{Prmd::VERSION}"
           exit(0)
         end
+        opts.on("-o", "--output-file FILENAME", String, "file to write result to") do |n|
+          options[:output_file] = n
+        end
         opts.separator "\nAvailable commands:"
         opts.separator help_text
       end
 
-      if argv.empty?
-        puts global
-        exit(1)
-      end
-      global.order!
+      begin
+        abort global if argv.empty?
 
-      command = argv.shift.to_sym
-      option = commands[command]
-      if option.nil?
-        puts global
-        exit(1)
+        com_argv = global.order!(argv)
+        command = com_argv.shift.to_sym
+        option = commands[command]
+
+        abort global if option.nil?
+        abort option if argv.empty? && $stdin.tty?
+
+        rem_argv = option.order!(com_argv)
+
+        options[:argv] = rem_argv
+        options[:command] = command
       end
 
-      if argv.empty? && $stdin.tty?
-        puts option
+      options
+    end
+
+    def self.write_result(data, options)
+      if output_file = options[:output_file]
+        File.open(output_file, "w") do |f|
+          f.write(data)
+        end
+      else
+        $stdout.puts data
+      end
+    end
+
+    def self.combine(paths, options={})
+      write_result Prmd.combine(paths, options).to_s, options
+    end
+
+    def self.doc(filename, options)
+      data = unless $stdin.tty?
+        $stdin.read
+      else
+        File.read(filename)
+      end
+      schema = Prmd::Schema.new(JSON.parse(data))
+
+      options[:template] = File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      write_result Prmd.render(schema, options), options
+    end
+
+    def self.init(name, options)
+      write_result Prmd.init(name, options), options
+    end
+
+    def self.render(filename, options)
+      data = unless $stdin.tty?
+        $stdin.read
+      else
+        File.read(filename)
+      end
+      schema = Prmd::Schema.new(JSON.parse(data))
+      write_result Prmd.render(schema, options), options
+    end
+
+    def self.verify(filename, options)
+      data, errors = '', []
+      unless $stdin.tty?
+        data = $stdin.read
+        errors.concat(Prmd.verify(JSON.parse(data)))
+      else
+        data = JSON.parse(File.read(filename))
+        Prmd.verify(data).each do |error|
+          errors << "#{filename}: #{error}"
+        end
+      end
+      unless errors.empty?
+        errors.each do |error|
+          $stderr.puts(error)
+        end
         exit(1)
       end
-      option.order!
+      $stdout.puts data unless $stdout.tty?
+    end
+
+    def self.run(uargv, opts={})
+      options = parse_options(uargv, opts)
+      argv = options.delete(:argv)
+      command = options.delete(:command)
 
       case command
-        when :combine
-          puts Prmd.combine(argv, options).to_s
-        when :doc
-          data = unless $stdin.tty?
-            $stdin.read
-          else
-            File.read(argv[0])
-          end
-          schema = Prmd::Schema.new(JSON.parse(data))
-
-          options[:template] = File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-
-          puts Prmd.render(schema, options)
-        when :init
-          puts Prmd.init(argv[0], options)
-        when :render
-          data = unless $stdin.tty?
-            $stdin.read
-          else
-            File.read(argv[0])
-          end
-          schema = Prmd::Schema.new(JSON.parse(data))
-          puts Prmd.render(schema, options)
-        when :verify
-          data, errors = '', []
-          unless $stdin.tty?
-            data = $stdin.read
-            errors.concat(Prmd.verify(JSON.parse(data)))
-          else
-            data = JSON.parse(File.read(argv[0]))
-            Prmd.verify(data).each do |error|
-              errors << "#{argv[0]}: #{error}"
-            end
-          end
-          errors.each do |error|
-            $stderr.puts(error)
-          end
-          exit(1) unless errors.empty?
-          puts(data) unless $stdout.tty?
+      when :combine
+        combine(argv, options)
+      when :doc
+        doc(argv.first, options)
+      when :init
+        init(argv.first, options)
+      when :render
+        render(argv.first, options)
+      when :verify
+        verify(argv.first, options)
       end
     end
   end
