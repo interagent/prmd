4144ffb0ac5cbefb1940f5f5ce0a6ee46c9fd185
Corey Powell <mistdragon100@gmail.com>
2014-09-22 14:21:45 -0500

    Cache templates

--- lib/prmd/template.rb
+++ lib/prmd/template.rb
@@ -1,23 +1,30 @@
 module Prmd
   class Template
-    def self.load(path, base)
-      fallback = File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+    @cache = {}
 
-      resolved = File.join(base, path)
-      if not File.exist?(resolved)
-        resolved = File.join(fallback, path)
-      end
-
-      return File.read(resolved)
+    def self.clear_cache
+      @cache.clear
     end
 
-    def self.render(path, base, *args)
-      template = self.load(path, base)
-      Erubis::Eruby.new(template).result(*args)
+    def self.load(path, base)
+      @cache[[path, base]] ||= begin
+        fallback = File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+        resolved = File.join(base, path)
+        if not File.exist?(resolved)
+          resolved = File.join(fallback, path)
+        end
+
+        Erubis::Eruby.new(File.read(resolved), filename: resolved)
+      end
     end
 
     def self.load_template(path, base)
-      Erubis::Eruby.new(load(path, base))
+      load(path, base)
+    end
+
+    def self.render(path, base, *args, &block)
+      load_template(path, base).result(*args, &block)
     end
   end
 end
