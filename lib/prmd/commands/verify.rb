0aa39a6f4372666b3db200c3ed4752c68318cda3
Corey Powell <mistdragon100@gmail.com>
2014-10-02 18:12:48 -0500

    Prmd.verify uses the Prmd::Verification module

--- lib/prmd/commands/verify.rb
+++ lib/prmd/commands/verify.rb
@@ -1,51 +1,77 @@
 require 'json'
 require 'json_schema'
 
+# :nodoc:
 module Prmd
-  # These schemas are listed manually and in order because they reference each
-  # other.
-  SCHEMAS = [
-    'schema.json',
-    'hyper-schema.json',
-    'interagent-hyper-schema.json'
-  ]
+  module Verification
+    # These schemas are listed manually and in order because they reference each
+    # other.
+    SCHEMAS = [
+      'schema.json',
+      'hyper-schema.json',
+      'interagent-hyper-schema.json'
+    ]
 
-  def self.verify(schema_data)
-    store = init_document_store
-
-    if !(schema_uri = schema_data['$schema'])
-      return ['Missing $schema.']
+    def self.init_document_store
+      store = JsonSchema::DocumentStore.new
+      SCHEMAS.each do |file|
+        file = File.expand_path("../../../../schemas/#{file}", __FILE__)
+        data = JSON.parse(File.read(file))
+        schema = JsonSchema::Parser.new.parse!(data)
+        schema.expand_references!(store: store)
+        store.add_schema(schema)
+      end
+      store
     end
 
-    # for good measure, make sure that the schema parses and that its
-    # references can be expanded
-    schema, errors = JsonSchema.parse!(schema_data)
-    return JsonSchema::SchemaError.aggregate(errors) if !schema
-
-    valid, errors = schema.expand_references(store: store)
-    return JsonSchema::SchemaError.aggregate(errors) if !valid
-
-    if !(meta_schema = store.lookup_schema(schema_uri))
-      return ["Unknown $schema: #{schema_uri}."]
+    def self.document_store
+      @document_store ||= init_document_store
     end
 
-    valid, errors = meta_schema.validate(schema_data)
-    return JsonSchema::SchemaError.aggregate(errors) if !valid
+    def self.verify_parsable(schema_data)
+      # for good measure, make sure that the schema parses and that its
+      # references can be expanded
+      schema, errors = JsonSchema.parse!(schema_data)
+      return JsonSchema::SchemaError.aggregate(errors) unless schema
 
-    []
+      valid, errors = schema.expand_references(store: document_store)
+      return JsonSchema::SchemaError.aggregate(errors) unless valid
+
+      []
+    end
+
+    def self.validate_meta_schema(meta_schema, schema_data)
+      valid, errors = meta_schema.validate(schema_data)
+      return JsonSchema::SchemaError.aggregate(errors) unless valid
+
+      []
+    end
+
+    def self.verify_schema(schema_data)
+      schema_uri = schema_data['$schema']
+      return ['Missing $schema key.'] unless schema_uri
+
+      meta_schema = document_store.lookup_schema(schema_uri)
+      return ["Unknown $schema: #{schema_uri}."] unless meta_schema
+
+      validate_meta_schema(meta_schema, schema_data)
+    end
+
+    def self.verify(schema_data)
+      a = verify_schema(schema_data)
+      return a unless a.empty?
+      b = verify_parsable(schema_data)
+      return b unless b.empty?
+      []
+    end
+
+    class << self
+      private :init_document_store
+      private :document_store
+    end
   end
 
-  private
-
-  def self.init_document_store
-    store = JsonSchema::DocumentStore.new
-    SCHEMAS.each do |file|
-      file = File.expand_path("../../../../schemas/#{file}", __FILE__)
-      data = JSON.parse(File.read(file))
-      schema = JsonSchema::Parser.new.parse!(data)
-      schema.expand_references!(store: store)
-      store.add_schema(schema)
-    end
-    store
+  def self.verify(schema_data)
+    Verification.verify(schema_data)
   end
 end
