f8b64be38c8e349b54e239f0f119faea5909d472
Corey Powell <mistdragon100@gmail.com>
2014-10-02 17:13:53 -0500

    HashHelpers is a submodule of Prmd

--- lib/prmd/hash_helpers.rb
+++ lib/prmd/hash_helpers.rb
@@ -1,13 +1,23 @@
-module HashHelpers
-  class << self
-    def deep_symbolize_keys(hash)
-      deep_transform_keys(hash){ |key| key.to_sym rescue key }
+# :nodoc:
+module Prmd
+  module HashHelpers
+    def self.deep_symbolize_keys(hash)
+      deep_transform_keys(hash) do |key|
+        if key.respond_to?(:to_sym)
+          key.to_sym
+        else
+          key
+        end
+      end
     end
 
-    def deep_transform_keys(hash, &block)
+    def self.deep_transform_keys(hash, &block)
       result = {}
       hash.each do |key, value|
-        result[yield(key)] = value.is_a?(Hash) ? deep_transform_keys(value, &block) : value
+        new_key = yield(key)
+        new_value = value
+        new_value = deep_transform_keys(value, &block) if value.is_a?(Hash)
+        result[new_key] = new_value
       end
       result
     end
