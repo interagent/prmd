33d1f8bcc6c824d52a37d75690b376aa88efe6c4
Corey Powell <mistdragon100@gmail.com>
2014-10-02 14:13:33 -0500

    load_schema_file, avoid loading the file before verifying it can be loaded
    tl;dr open first, check extension and then read

--- lib/prmd/load_schema_file.rb
+++ lib/prmd/load_schema_file.rb
@@ -3,16 +3,17 @@ require 'json'
 
 module Prmd
   def self.load_schema_file(filename)
-    data = File.read(filename)
     extname = File.extname(filename)
-
-    case extname.downcase
-    when '.yaml', '.yml'
-      YAML.load(data)
-    when '.json'
-      JSON.load(data)
-    else
-      abort "Cannot load schema file #{filename}, (unsupported file extension #{extname})"
+    File.open(filename) do |file|
+      case extname.downcase
+      when '.yaml', '.yml'
+        YAML.load(file.read)
+      when '.json'
+        JSON.load(file.read)
+      else
+        abort "Cannot load schema file #{filename}" \
+              "(unsupported file extension #{extname})"
+      end
     end
   end
 end
