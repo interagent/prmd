fad5584571065ae24957a9ff1c4f195ef4a5bcab
Adam Saegebarth <adams@tune.com>
2015-08-19 14:55:30 -0700

    modify schema to support integers in the urls

--- lib/prmd/version.rb
+++ lib/prmd/version.rb
@@ -2,7 +2,7 @@
 module Prmd
   # Well, duh, its a Version module, what did you expect?
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 7, 4, nil
+    MAJOR, MINOR, TEENY, PATCH = 0, 7, 4, 1
     # version string
     # @return [String]
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
