03d759e5f206182cfe168ddb33336a67c5c3b2f8
Brian McManus <bdmac97@gmail.com>
2015-10-20 13:35:46 -0700

    Allow custom init template to be used

--- lib/prmd/commands/init.rb
+++ lib/prmd/commands/init.rb
@@ -9,9 +9,21 @@ module Prmd
     # Creates a default Prmd::Generator using default templates
     #
     # @return [Prmd::Generator]
-    def self.make_generator
+    def self.make_generator(options)
       base = Prmd::Template.load_json('init_default.json')
-      template = Prmd::Template.load_template('init_resource.json.erb', '')
+      template_name = options.fetch(:template) do
+        abort 'render: Template was not provided'
+      end
+      if template_name && !template_name.empty?
+        template_dir = File.expand_path(template_name)
+        # to keep backward compatibility
+        template_dir = File.dirname(template_name) unless File.directory?(template_dir)
+        template_name = File.basename(template_name)
+      else
+        template_name = 'init_resource.json.erb'
+        template_dir  = ''
+      end
+      template = Prmd::Template.load_template(template_name, template_dir)
       Prmd::Generator.new(base: base, template: template)
     end
   end
@@ -22,7 +34,7 @@ module Prmd
   # @param [Hash<Symbol, Object>] options
   # @return [String] schema template in YAML (yaml option was enabled) else JSON
   def self.init(resource, options = {})
-    gen = Generate.make_generator
+    gen = Generate.make_generator(template: options[:template])
 
     generator_options = { resource: nil, parent: nil }
     if resource
