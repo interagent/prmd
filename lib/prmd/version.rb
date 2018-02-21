3c430d47c5886af938e05c24f5fa055a63938736
ginkouno <gh.ginkouno@gmail.com>
2018-02-21 21:51:05 +0900

    bump up version k2

--- lib/prmd/version.rb
+++ lib/prmd/version.rb
@@ -2,7 +2,7 @@
 module Prmd
   # Well, duh, its a Version module, what did you expect?
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 13, 0, 'k1'
+    MAJOR, MINOR, TEENY, PATCH = 0, 13, 0, 'k2'
     # version string
     # @return [String]
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
